01263f63b65d28f1fa69ea7cda6f803e4bac9e03
Cameron Tanjoco <ctanjoco@gmail.com>
2022-04-05 20:22:48 -0700

    was able to get letter_combinations

--- BackTracking/letter_combinations.js
+++ BackTracking/letter_combinations.js
@@ -60,3 +60,40 @@ var letterCombinations = function (digits) {
  * and return
  *
  */
+
+/**
+ * Update 4/5/22
+ *
+ * Was able to get it
+ */
+
+var letterCombinations = function (digits) {
+  let result = [];
+  let map = {
+    2: ["a", "b", "c"],
+    3: ["d", "e", "f"],
+    4: ["g", "h", "i"],
+    5: ["j", "k", "l"],
+    6: ["m", "n", "o"],
+    7: ["p", "q", "r", "s"],
+    8: ["t", "u", "v"],
+    9: ["w", "x", "y", "z"],
+  };
+
+  let backtrace = (index, currStr) => {
+    if (currStr.length === digits.length) {
+      result.push(currStr);
+      return;
+    }
+
+    for (let char of map[digits[index]]) {
+      backtrace(index + 1, currStr + char);
+    }
+  };
+
+  if (digits) {
+    backtrace(0, "");
+  }
+
+  return result;
+};
