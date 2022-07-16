e13f5a3bfcbc95dc74f5d0a450977155455a67fa
Cameron Tanjoco <ctanjoco@gmail.com>
2022-07-15 18:14:28 -0700

    product_of_array_except_self

---
+++ Array/product_of_array_except_self.js
@@ -0,0 +1,64 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function (nums) {
+  /*
+        two-pointers
+            - except that they start at position 0
+        
+        i, j = 0
+        
+        let result = []
+        iterate through the array via i
+            - tempVal = 1 to multiply val to
+            iterate through array via j
+                if i==j continue
+                    -  cannot multiply self
+                tempVal = tempVal * nums[j]
+            - push tempVal into result
+    */
+
+  /*
+        nums = [1,2,3,4]
+        result = [24,]
+        i = 0 1
+        j = 0 1 2 3 0 1 2 
+        tempVal = 1 2 6 24 1 1 3 12 
+        nums[j] = 2 3 4 1 
+    */
+  let result = [];
+  for (let i = 0; i < nums.length; i++) {
+    let tempVal = 1;
+    for (let j = 0; j < nums.length; j++) {
+      if (i === j) continue;
+      tempVal = tempVal * nums[j];
+    }
+    result.push(tempVal);
+  }
+
+  return result;
+};
+
+/*
+    the fastest js version
+    var productExceptSelf = function(nums) {
+    const result = [];
+    let productSoFar = 1;
+    for (let i = 0; i < nums.length; i++) {
+        result[i] = productSoFar
+        productSoFar *= nums[i]
+    }
+    productSoFar = 1
+    for (let j = nums.length-1; j >= 0; j--) {
+        result[j] *= productSoFar
+        productSoFar *= nums[j]
+    }
+    return result;
+};
+    nums = [1,2,3,4]
+    result = [1] [1, 1] [1, 1, 2] [1,1,2,6]
+    productSoFar = 1 1 2 6
+    nums[i] = 1 2 3 4 
+    i = 0 1 2 3
+*/
