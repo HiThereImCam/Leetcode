d6d379b27f08f9bb50591398a8ba414eced8bd41
Cameron Tanjoco <ctanjoco@gmail.com>
2022-07-27 08:47:17 -0700

    depth_first_values.js

---
+++ Trees/depth_first_values.js
@@ -0,0 +1,35 @@
+/*
+              a
+            b   c
+         
+         
+         
+*/
+const depthFirstValues = (root) => {
+  if (!root) return [];
+
+  let leftValues = depthFirstValues(root.left);
+  let rightValues = depthFirstValues(root.right);
+
+  return [root.val, ...leftValues, ...rightValues];
+};
+
+const depthFirstValuesIteratively = (root) => {
+  if (!root) return [];
+  // iteratively
+  let result = [];
+  let stack = [root];
+
+  while (stack.length > 0) {
+    let currNode = stack.pop();
+    result.push(currNode.val);
+    if (currNode.right) {
+      stack.push(currNode.right);
+    }
+    if (currNode.left) {
+      stack.push(currNode.left);
+    }
+  }
+
+  return result;
+};
