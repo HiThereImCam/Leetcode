7000688f79cb43bd99e7b1d15a8d4d6855ef9867
Cameron Tanjoco <ctanjoco@gmail.com>
2022-07-27 09:22:13 -0700

    tree_sum.js

---
+++ Trees/tree_sum.js
@@ -0,0 +1,24 @@
+const treeSum = (root) => {
+  if (!root) return 0;
+  let sum = 0;
+  let queue = [root];
+
+  while (queue.length > 0) {
+    let currNode = queue.shift();
+    sum += currNode.val;
+
+    if (currNode.left) queue.push(currNode.left);
+    if (currNode.right) queue.push(currNode.right);
+  }
+
+  return sum;
+};
+
+const treeSumRecursively = (root) => {
+  if (!root) return 0;
+
+  let leftSum = treeSum(root.left);
+  let rightSum = treeSum(root.right);
+
+  return root.val + leftSum + rightSum;
+};
