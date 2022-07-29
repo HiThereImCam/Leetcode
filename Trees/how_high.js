47aff4d8c4ea9cb0e7adf536ee6d76f64e97adb2
Cameron Tanjoco <ctanjoco@gmail.com>
2022-07-28 18:47:50 -0700

    how_high.js

--- Trees/how_high.js
+++ Trees/how_high.js
@@ -4,3 +4,14 @@
 
 
 */
+const howHigh = (node) => {
+  /*
+            1
+          2   3
+        4  5 6 7
+               8
+    */
+  if (!node) return -1; // height of one node is 0
+
+  return 1 + Math.max(howHigh(node.left), howHigh(node.right));
+};
