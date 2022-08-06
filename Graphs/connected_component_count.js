630e0e0f9fbb7f6cde5483debc2b3c559504120c
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-05 21:25:23 -0700

    connected_component_count part2

--- Graphs/connected_component_count.js
+++ Graphs/connected_component_count.js
@@ -60,3 +60,47 @@ const explore = (graph, current, visited) => {
 
   return true;
 };
+
+/*
+  did again
+    - i didn't know what to return
+      - boolean
+    - should do it again
+
+*/
+
+const connectedComponentsCount2 = (graph) => {
+  const visited = new Set();
+  let count = 0;
+  for (let node in graph) {
+    if (explore(graph, node, visited) === true) {
+      count += 1;
+    }
+  }
+
+  //return count
+
+  console.log("count: ", count);
+};
+
+const explore2 = (graph, current, visited) => {
+  if (visited.has(String(current))) return false;
+
+  visited.add(String(current));
+
+  for (let neighbor of graph[current]) {
+    explore(graph, current, visited);
+  }
+
+  return true;
+};
+
+connectedComponentsCount({
+  0: [8, 1, 5],
+  1: [0],
+  5: [0, 8],
+  8: [0, 5],
+  2: [3, 4],
+  3: [2, 4],
+  4: [3, 2],
+});
