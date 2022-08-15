94f7045ef1c38607b52b8cf052fb5c516500217d
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-15 09:55:24 -0700

    group_anagrams.js

---
+++ Array/group_anagrams.js
@@ -0,0 +1,33 @@
+// n log n
+const groupAnagrams = (strs) => {
+  let result = {};
+
+  for (let i = 0; i < strs.length; i++) {
+    let sortedStr = strs[i].split("").sort().join(",");
+    if (result[sortedStr]) {
+      result[sortedStr].push(strs[i]);
+    } else {
+      result[sortedStr] = [strs[i]];
+    }
+  }
+
+  return Object.values(result);
+};
+
+// O(m * n) where m is length of strs and n is length of str
+const groupAnagrams2 = (strs) => {
+  let result = {};
+
+  for (let str of strs) {
+    let count = new Array(26).fill(0);
+
+    for (let char of str) {
+      count[char.charCodeAt(0) - 97] += 1;
+    }
+
+    if (!result[count]) result[count] = [];
+    result[count].push(str);
+  }
+
+  return Object.values(result);
+};
