cbc9ab233c5b3df7927e8a9f45cdb41fda4cfe4b
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-16 07:32:12 -0700

    subsets_part2

--- BackTracking/subset.js
+++ BackTracking/subset.js
@@ -21,3 +21,31 @@ const subsets = (elements) => {
 
   return [...subWithFirst, ...subWithoutFirst];
 };
+
+// part 2 on 8/16
+
+/*
+    base case
+        if array empty then return [[]]
+        
+    we can choose not to add the curr val
+    and to add the curr val
+    
+    
+    
+    nums = [1, 2, 3]
+*/
+var subsets2 = function (nums) {
+  if (nums.length < 1) return [[]];
+
+  let currEle = nums[0];
+  let restOfElements = nums.slice(1);
+  let arrWithoutFirst = subsets(restOfElements); // [[], [2], [3]]
+  let arrWithFirst = [];
+
+  for (let sub of arrWithoutFirst) {
+    arrWithFirst.push([currEle, ...sub]);
+  }
+
+  return [...arrWithFirst, ...arrWithoutFirst];
+};
