33a457473ca44c3ca78cd3ba5383947f1ee8846f
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-24 15:59:51 -0700

    coin_change_2.js

--- DP/min_coin_change.js
+++ DP/min_coin_change.js
@@ -17,3 +17,33 @@ const _minChange = (amount, coins, memo = {}) => {
   }
   return (memo[amount] = min);
 };
+
+// tried again. couldnt get it
+// 08/24
+
+/*
+  if amount = 0
+    I want to return the count
+  
+  I want to iterate through the array
+  
+  simplest case is 
+  target 10 [8, 2]
+  return 1
+  
+  how do I keep track of the amount of coins needed?
+  if amount === 0, what am I returning?
+    - returning 0
+      - the minimum amount of coins to make 0 cents is 0 coins
+  
+  how does the memo come into play?
+  what I storing as the key value pair?
+*/
+
+const minChange2 = (amount, coins) => {
+  // iterate through the coins
+  if (amount === 0) let;
+  for (let coin of coins) {
+    let currentCount = 1 + minChange(amount - coin, coins);
+  }
+};
--- DP/tribonacci.js
+++ DP/tribonacci.js
@@ -8,3 +8,15 @@ const tribonacci = (n) => {
   memo[n] = tribonacci(n - 1) + tribonacci(n - 2) + tribonacci(n - 3);
   return memo[n];
 };
+
+// part 2
+function tribonccci2(num, memo = {}) {
+  if (num === 0 || num === 1) return 0;
+  if (num === 2) return 1;
+
+  if (num in memo) return memo[num];
+
+  memo[num] = tribonacci(num - 3) + tribonacci(num - 2) + tribonacci(num - 1);
+
+  return memo[num];
+}
