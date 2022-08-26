5f0efaae2c21973e5d7fb4258718e3ca1f9d3aa0
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-26 12:55:00 -0700

    non_adjacent_sum.js

---
+++ DP/non_adjacent_sum.js
@@ -0,0 +1,10 @@
+const nonAdjacentSum = (nums, i = 0, memo = {}) => {
+  if (i in memo) return memo[i];
+  if (i >= nums.length) return 0;
+
+  let include = nums[i] + nonAdjacentSum(nums, i + 2, memo);
+  let exclude = nonAdjacentSum(nums, i + 1, memo);
+
+  memo[i] = Math.max(include, exclude);
+  return memo[i];
+};
