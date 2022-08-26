c7f113e7387347195f91b7dedad86de5ba38746e
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-26 10:01:16 -0700

    valid_sudoku part 2

--- Array/valid_sudoku.js
+++ Array/valid_sudoku.js
@@ -76,3 +76,45 @@ var isValidSudoku = function (board) {
     Time Complexity = O(9^2)
     Space Complexity = O(9 ^ 2)
 */
+
+// did this again on 8/26/22
+// got it
+
+var isValidSudoku2 = function (board) {
+  let row = {};
+  let col = {};
+  let square = {};
+
+  for (let r = 0; r < 9; r++) {
+    for (let c = 0; c < 9; c++) {
+      let currVal = board[r][c];
+
+      if (currVal === ".") continue;
+
+      let grid = `${Math.floor(r / 3)}${Math.floor(c / 3)}`;
+
+      if (!row[r]) {
+        row[r] = new Set();
+      }
+      if (!col[c]) {
+        col[c] = new Set();
+      }
+      if (!square[grid]) {
+        square[grid] = new Set();
+      }
+
+      if (
+        row[r].has(currVal) ||
+        col[c].has(currVal) ||
+        square[grid].has(currVal)
+      ) {
+        return false;
+      }
+
+      row[r].add(currVal);
+      col[c].add(currVal);
+      square[grid].add(currVal);
+    }
+  }
+  return true;
+};
