f690ab376129227d3ec05545df64a3da1d3763b1
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-31 08:56:44 -0700

    good_nodes parts 2

--- Trees/count_good_nodes_in_bt.js
+++ Trees/count_good_nodes_in_bt.js
@@ -29,7 +29,7 @@
             2       3
         4
 */
-
+// BAD
 var goodNodes = function (root, prevVal = root.val) {
   if (!root) return 0;
 
@@ -40,6 +40,50 @@ var goodNodes = function (root, prevVal = root.val) {
 
   let left = goodNodes(root.left, root.val);
   let right = goodNodes(root.right, root.val);
+
+  return count + left + right;
+};
+
+// 08/31/2022 passed
+
+/*
+            3
+         1     4
+              2
+                5
+
+    node val: 3
+    prev: 3
+    count: 1
+    left: 0 
+    right:2
+    
+    returns 3 
+    
+            9
+                3 
+                    6 
+            
+    node val: 9
+    prev: 9
+    count: 1
+    left: 0
+    right: 0
+    returns 1
+        
+                    
+*/
+
+var goodNodes2 = function (root, prev = root.val) {
+  if (!root) return 0;
+
+  let count = 0;
+  if (root.val >= prev) {
+    count += 1;
+  }
+
+  let left = goodNodes2(root.left, Math.max(prev, root.val));
+  let right = goodNodes2(root.right, Math.max(prev, root.val));
 
   return count + left + right;
 };
