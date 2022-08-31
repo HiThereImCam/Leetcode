1955bd4aefa9f190f2d848204250d1bb23a36e8d
Cameron Tanjoco <ctanjoco@gmail.com>
2022-08-30 19:34:12 -0700

    same_tree part 2

--- Trees/same_tree.js
+++ Trees/same_tree.js
@@ -11,3 +11,18 @@ var isSameTree = function (p, q) {
   }
   return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
 };
+
+// 08/30/2022
+var isSameTree2 = function (p, q) {
+  if (p === q) return true;
+  if (p === null && q !== null) {
+    return false;
+  }
+  if (p !== null && q === null) {
+    return false;
+  }
+  if (p.val !== q.val) {
+    return false;
+  }
+  return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+};
