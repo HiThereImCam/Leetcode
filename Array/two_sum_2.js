814edeccbdbbd7468867002a6889efdc064c572d
Cameron Tanjoco <ctanjoco@gmail.com>
2022-09-02 20:52:56 -0700

    two_sum_II part 2

--- Array/two_sum_2.js
+++ Array/two_sum_2.js
@@ -46,3 +46,50 @@ var twoSum = function (numbers, target) {
     key here is 2 pointer because of sorted array
     
 */
+
+// 09/02/22
+/*
+    [2 7 11 15]
+     i j 
+    
+    
+    [2 3 4] target 6
+     i   j 
+     
+     [7 8 9] target = 17
+        i j
+    
+     [-4, -3, -1] target = -2 
+     
+     is Math.abs(i) > target?
+        - move i up
+     is Math.abs(j) > target
+        - move j down
+    
+    [-4 -3 -1] target = -2 
+    
+    nums[i] = -3
+    nums[j] = -1
+    
+    returns [2, 3]
+*/
+
+var twoSum2 = function (numbers, target) {
+  let i = 0;
+  let j = numbers.length - 1;
+
+  while (i <= Math.floor(numbers.length / 2)) {
+    //if(Math.abs(target) - Math.abs(numbers[i]) === numbers[j]) return [i + 1, j + 1]
+    if (Math.abs(numbers[i]) > target) {
+      let diff = Math.abs(numbers[i]) - Math.abs(target);
+      if (diff === numbers[j]) return [i + 1, j + 1];
+      i += 1;
+      continue;
+    } else {
+      if (Math.abs(target) - Math.abs(numbers[i]) === numbers[j])
+        return [i + 1, j + 1];
+      j -= 1;
+      continue;
+    }
+  }
+};
