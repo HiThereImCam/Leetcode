b6c42da9e8072a70e938c8472218875ec6b9ca57
Cameron Tanjoco <ctanjoco@gmail.com>
2022-09-13 14:32:19 -0700

    lowest_common_ancestor.js

--- Trees/lowest_common_ancestor_bst.js
+++ Trees/lowest_common_ancestor_bst.js
@@ -56,3 +56,61 @@ var lowestCommonAncestor = function (root, p, q) {
  * did not get it. will try again on 5/3/2022
  *
  */
+
+/*
+  idea
+    - keep track of previous parents?
+    
+  we could create an obj because of O(1) look up and O(1) insertion
+  
+  {
+    a: null       // b/c it's the root
+    b: a
+    d: b
+    e: b
+    h: e 
+  }
+  
+  helper function
+  we could traverse through the tree in breadth first
+    - as we traverse it feels like
+      queue = [[node, prev]]
+      so starting it would be [[root, null]]
+  
+  once our obj is filled 
+  we can iterate through the obj until we hit null or we hit a common ancestor
+  
+  
+  dfs 
+*/
+
+const lowestCommonAncestor = (root, val1, val2) => {
+  let path1 = getPath(root, val1);
+  let path2 = getPath(root, val2);
+  let set = new Set(path2);
+
+  for (let val of path1) {
+    if (set.has(val)) {
+      return val;
+    }
+  }
+};
+
+const getPath = (root, target) => {
+  if (!root) return null;
+  if (root.val === target) return [root.val];
+
+  let leftPath = getPath(root.left, target);
+  if (leftPath !== null) {
+    leftPath.push(root.val);
+    return leftPath;
+  }
+
+  let rightPath = getPath(root.right, target);
+  if (rightPath !== null) {
+    rightPath.push(root.val);
+    return rightPath;
+  }
+
+  return null;
+};
