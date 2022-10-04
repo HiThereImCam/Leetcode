b7e6ea994cd63de39d171a11676a7bee9f3ff2ea
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-04 08:39:38 -0700

    valid_sudoku.js

--- Array/valid_sudoku.js
+++ Array/valid_sudoku.js
@@ -118,3 +118,88 @@ var isValidSudoku2 = function (board) {
   }
   return true;
 };
+
+// 10/04/2022
+/*
+    returning a boolean
+
+    [
+        c0  c1
+        [1, 3]          // row 0
+        [2, 4]          // row 1
+    ]
+    
+    note:
+        - I can set a Set to the row, col to ensure uniqueness
+    
+    Questions that need to be answered:
+        - how can I set a Set to subGrid
+            - identify which subGrid
+            - Set where the key is going to be Math.floor(r/3) && Math.floor(c/3)
+    
+    create variables of row, col, grid set to obj
+        - their key: idx val is set
+    
+    iterate through the grid
+        -  if !row has set
+            - create Set for row
+        -  if !col has set
+            - create Set for col
+        - if !subGrid has set
+            - create Set for subGrid
+            
+        - check if row has val
+            - if yes, we know theres a dup. Return false
+        - check if col has val
+            - if yes, return false
+        - check if subGrid has val
+            - if yes return false
+            
+    
+    after finishing running through the whole grid
+    return true
+        
+
+*/
+
+var isValidSudoku = function (board) {
+  let row = {};
+  let col = {};
+  let subBoard = {};
+
+  for (let r = 0; r < board.length; r += 1) {
+    for (let c = 0; c < board[0].length; c += 1) {
+      let currEle = board[r][c];
+
+      if (currEle === ".") continue;
+
+      if (!(r in row)) {
+        row[r] = new Set();
+      }
+
+      if (!(c in col)) {
+        col[c] = new Set();
+      }
+
+      let subGrid = Math.floor(r / 3) + "," + Math.floor(c / 3);
+
+      if (!(subGrid in subBoard)) {
+        subBoard[subGrid] = new Set();
+      }
+
+      if (
+        row[r].has(currEle) ||
+        col[c].has(currEle) ||
+        subBoard[subGrid].has(currEle)
+      ) {
+        return false;
+      }
+
+      row[r].add(currEle);
+      col[c].add(currEle);
+      subBoard[subGrid].add(currEle);
+    }
+  }
+
+  return true;
+};
