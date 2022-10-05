0bed4bb19e37eefd65cc9f5abb1cce1dd6cff1d3
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-04 19:32:24 -0700

    meeting_rooms_ii.js

--- Array/meeting_rooms_II.js
+++ Array/meeting_rooms_II.js
@@ -20,3 +20,40 @@ var minMeetingRooms = function (intervals) {
 
   return finalCount;
 };
+
+/**
+ * 10/4/2022
+ */
+
+const getSortedArray = (intervals, idx) => {
+  let result = [];
+
+  for (let i = 0; i < intervals.length; i++) {
+    result.push(intervals[i][idx]);
+  }
+
+  return result.sort();
+};
+
+var minMeetingRooms = function (intervals) {
+  const startArr = getSortedArray(intervals, 0);
+  const endArr = getSortedArray(intervals, 1);
+
+  let count = 0;
+  let finalCount = 0;
+  let startIdx = 0;
+  let endIdx = 0;
+
+  while (startIdx < startArr.length) {
+    if (startArr[startIdx] < endArr[endIdx]) {
+      count += 1;
+      finalCount = Math.max(count, finalCount);
+      startIdx += 1;
+    } else {
+      count -= 1;
+      endIdx += 1;
+    }
+  }
+
+  return finalCount;
+};
