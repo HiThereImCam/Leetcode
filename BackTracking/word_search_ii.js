191bd029d82d545e5275701f00c912d25b516182
Cameron Tanjoco <ctanjoco@gmail.com>
2022-10-24 16:01:35 -0700

    word_search_ii part 2

--- BackTracking/word_search_ii.js
+++ BackTracking/word_search_ii.js
@@ -58,3 +58,65 @@ const findWords = (board, words) => {
 
   return result;
 };
+
+// part 2
+var findWords2 = function (board, words) {
+  let result = [];
+
+  /*
+      a trie has
+          - a root
+          - children
+          - a property that indicates the end of the word
+  */
+  const buildTrie = () => {
+    let root = {};
+
+    for (let word of words) {
+      let currNode = root;
+      for (let char of word) {
+        if (!currNode[char]) currNode[char] = {};
+
+        // update node
+        currNode = currNode[char];
+      }
+      currNode.word = word;
+    }
+
+    return root;
+  };
+
+  const traverseBoard = (row, col, currNode) => {
+    if (currNode.word !== null && currNode.word !== undefined) {
+      result.push(currNode.word);
+      currNode.word = null;
+    }
+
+    if (row < 0 || row >= board.length || col < 0 || col >= board[0].length)
+      return;
+
+    if (currNode[board[row][col]] == undefined) return;
+    if (board[row][col] === "#") return;
+
+    let currChar = board[row][col];
+    board[row][col] = "#";
+
+    traverseBoard(row, col - 1, currNode[currChar]);
+    traverseBoard(row, col + 1, currNode[currChar]);
+    traverseBoard(row - 1, col, currNode[currChar]);
+    traverseBoard(row + 1, col, currNode[currChar]);
+
+    board[row][col] = currChar;
+  };
+
+  let root = buildTrie();
+  // console.dir(root, {depth: null})
+
+  for (let row = 0; row < board.length; row += 1) {
+    for (let col = 0; col < board[0].length; col += 1) {
+      traverseBoard(row, col, root);
+    }
+  }
+
+  return result;
+};
