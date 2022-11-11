067373fa31db5b96b9dcd1dd1068335e20ccd0e4
HiThereImCam <ctanjoco@gmail.com>
2022-11-11 12:09:41 -0800

    semesters required part 3

--- Graphs/semesters_required.js
+++ Graphs/semesters_required.js
@@ -90,3 +90,87 @@ const semestersRequired = (numCourses, prereqs) => {
   
   watch out for []
 */
+
+
+// 11/11/2022
+/*
+  we can build a graph via  {}
+  
+  {
+    0: [5],
+    1: [2],
+    2: [4],
+    3: [5],
+    4: [],
+    5: []
+  }
+  
+  constraint
+    - can there be cycles?
+  
+  what do we need?
+    - visited Set
+    - count 
+  
+  do I want the starting points?
+    - if I have all the starting points aka keys that are not empty
+        - iterate through all of the keys until i get to an key
+          that does not have any children
+        - update the count 
+        
+  idea
+    - create a graph
+    - create a queue of starting points
+    - iterate through the queue
+    - traverse through the children until I hit a child
+      with no children
+    - return count
+*/
+
+const createGraph2 = (numCourses, prereqs) => {
+  let graph = {}
+  
+  for(let i = 0; i < numCourses; i += 1){
+    graph[i] = []
+  }
+  
+  for(let pair of prereqs){
+    let [start, end] = pair
+    graph[start].push(end)
+  }
+  return graph
+}
+
+// are there any other edge cases?
+// how do I add?
+// i'm not sure how to add
+
+const traverse = (graph, currCourse, distance) => {
+  if(currCourse in distance) return distance[currCourse]
+  
+  let maxCount = 0
+  for(let neighbor of graph[currCourse]){
+    let currCount = traverse(graph, neighbor, distance)
+    maxCount = Math.max(maxCount, currCount)
+  }
+
+  distance[currCourse] = 1 + maxCount
+  return distance[currCourse]
+}
+
+const semestersRequired2 = (numCourses, prereqs) => {
+  const graph = createGraph(numCourses, prereqs)
+  console.log(graph)
+  // find the terminal node
+  let distance = {}
+  
+  for(let node in graph){
+    if(graph[node].length < 1) distance[node] = 1
+  }
+  
+  for(let course in graph){
+    traverse(graph, course, distance)
+  }
+  
+  return Math.max(...Object.values(distance))
+};
