6cccf40be9bf34c162254ca64d069e0331f34572
HiThereImCam <ctanjoco@gmail.com>
2022-11-18 21:48:30 -0800

    decompress braces

--- Stack/decompress_braces.js
+++ Stack/decompress_braces.js
@@ -33,3 +33,96 @@ const decompressBraces = (s) => {
 
   return stack.join("");
 };
+
+
+// 11/18/2022
+
+/*
+  KEEP IN MIND
+    - COMPRESSION FORMAT IS 'n{subString}'
+  idea
+    - possibilities to look for
+      - open curly braces because thats the start of a substring
+      - stack data stucture to keep track of decompressed string
+      - create a variable to keep track of the NUMBER 
+      - create a helper function to concatenate the string n 
+        number of times
+      - 
+  
+  
+  questions that we have to think about 
+    - what happens when there are nested substrings
+      
+  
+ ex: 2{q} -> qq
+      - iterating through the string
+      - i see an open brace
+         - crreate a sub string
+         
+  What I couldn't figure out:
+    - how do I handle the compressed string
+      - i did have an idea of adding values to the output
+        array and then popping them off
+    
+  Questions:
+    - how to handle multiple layers of compression
+      - string 
+      - number
+      
+  Answer:
+    - I was using the stack wrong
+  
+  "2{q}3{tu}v"
+   at 2 -> stack [2]
+   at { -> do nothing
+   at q -> stack [2, q]
+   at } -> we now pop off stack
+*/
+
+const strConcat = (number, str) => {
+  let iter = 0
+  let newStr = ''
+  while(iter < number){
+    newStr += str 
+    iter += 1
+  }
+  
+  return newStr
+}
+
+/*
+    "2{q}3{tu}v"
+    
+    stack = [qq, tututu, v]
+    
+*/
+
+const decompressBraces3 = (s) => {
+  const numbers = '123456789'
+  let stack = []
+   
+  
+  
+  for(let char of s){
+    if(char === '{'){
+      continue
+    }else{
+      if(char === '}'){
+        let str = ''
+        while(!numbers.includes(stack[stack.length - 1])){
+          const popped = stack.pop()
+          str = popped + str
+        }
+        let number = parseInt(stack.pop())
+        let decompressed = strConcat(number,str)
+        stack.push(decompressed)
+      } else{
+        stack.push(char)
+      } 
+      console.log(stack)
+    }
+  }
+  
+   // console.log('str: ', stack.join(''))
+  return stack.join('')
+};
