058c70dc4bfe4b2efe850f222bf381d715693dc4
HiThereImCam <ctanjoco@gmail.com>
2022-11-25 10:49:30 -0800

    Meeting rooms 2 part 3"
    ;

--- Array/meeting_rooms_II.js
+++ Array/meeting_rooms_II.js
@@ -57,3 +57,61 @@ var minMeetingRooms = function (intervals) {
 
   return finalCount;
 };
+
+
+// 11/25/2022
+/*
+    question
+        - [[5,10], [15,20], [0,30]]
+                       i
+                       
+        - how do we know that this is going 
+        - we know that we do not have to add a count to the number meetings rooms
+          because the current interval start time > prev intervals end time 
+          
+        - [[7, 10], [2,4]]
+        - [[2,4], [3,10]] => 2
+        - [[2,4], [7,10]] => 1
+        
+    
+    [[2,4], [7,10]]
+               i
+       
+    prev = 4
+    count = 1
+    
+    
+*/
+
+const sortArr = (arr, idx) => {
+  const result = []
+  
+  for(let val of arr){
+      result.push(val[idx])
+  }
+  
+  return result.sort((a,b) => a - b)
+}
+
+var minMeetingRooms = function(intervals) {
+  const startArr = sortArr(intervals, 0)
+  const endArr = sortArr(intervals, 1)
+  
+  let startIdx = 0
+  let endIdx = 0
+  let count = 0
+  let finalCount = 0
+  
+  while(startIdx < startArr.length){
+      if(startArr[startIdx] < endArr[endIdx]){
+          count += 1
+          finalCount = Math.max(count, finalCount)
+          startIdx += 1
+      }else{
+          count -= 1
+          endIdx += 1
+      }
+  }
+  
+  return finalCount
+};
