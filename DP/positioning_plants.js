3837eb8ede114da0ffd53cf941a6786576ac4720
HiThereImCam <ctanjoco@gmail.com>
2022-11-27 17:42:09 -0800

    positioning_plantspart 2

--- DP/positioning_plants.js
+++ DP/positioning_plants.js
@@ -50,3 +50,67 @@ const positioningPlants = (costs, pos = 0, lastPlant = null, memo = {}) => {
 //   [6, 1, 9],
 //   [2, 5, 3]
 // ]);
+
+
+// 11/27/2022
+/*
+  we cannot have 2 flowers of the same type planted next to each other
+  
+  total cost for planting at position 1 (idx 1) = $6 + $1 + $9
+  
+  i = row = position
+  j = col = flower type
+  costs[i][j] = cost of planting the flower type j at position i
+  
+  return the minimum cost we need to plant a flower in each position of
+  the garden
+  
+  constraints
+    -  I assume that there will be non-negative numbers
+    - I assume that there will be non-zero numbers
+    - 
+    
+    
+                          [curr_row, prev_col]
+                          
+                 4 /          3|          \ 7
+              [1, 9]         [6,9]        [1,7]
+             1 /  \ 9
+          [2,3]
+        2 /  \ 3
+      7        8
+      
+      
+  DYN PROG
+  
+  recursively
+    - what is my base case?
+      - if my current row > costs.length 
+        - return 0
+      - 
+  
+  what do i want to do? 
+    - I want to iterate through the array 
+      - single loop
+    
+    
+*/
+
+const positioningPlants2 = (costs, pos = 0, lastPlant = null, memo={}) => {
+  const key = pos + ',' + lastPlant;
+  if(key in memo) return memo[key];
+  
+  if(pos === costs.length) return 0;
+  
+  let min = Infinity;
+  for(let plant = 0; plant < costs[pos].length; plant += 1){
+    if(plant !== lastPlant){
+      const candidate = costs[pos][plant] + positioningPlants(costs, pos + 1, plant, memo)
+     min = Math.min(min, candidate)      
+    } 
+  }
+  
+  
+  memo[key] = min
+  return min
+};
