172c75142872963d5f45e53fe4ed3a0d24832090
HiThereImCam <ctanjoco@gmail.com>
2022-11-28 19:09:17 -0800

    max increasing subsequence 2

--- DP/max_increasing_subseq.js
+++ DP/max_increasing_subseq.js
@@ -91,3 +91,50 @@ const maxIncreasingSubseq = (numbers, idx = 0, prev = -Infinity, memo = {}) => {
   memo[key] = Math.max(...options);
   return memo[key];
 };
+
+
+// 11/28/2022
+/*
+  [4, 18, 20, 10, 12, 15, 19];
+          
+                              4
+                    18  20  10  12  15  19
+                20          12  15  19  
+                            15  19
+                            19
+                
+                
+                
+    [18, 20, 10]
+  
+  - the count does include the current number
+  
+  decision tree
+    - base case 
+      - if the prevVal > currVal
+          return the currCount
+      - iterate through the array
+          make a recursive call 
+    
+    
+    how do I take and not take?
+      -
+*/ 
+
+const maxIncreasingSubseq2 = (numbers, i = 0, prev = -Infinity, memo = {}) => {
+  const key = i + ',' + prev;
+  if(key in memo) return memo[key]
+  if(i === numbers.length) return 0
+  
+  const current = numbers[i];
+  const options = []
+  const dontTakeCurrent = maxIncreasingSubseq(numbers, i + 1, prev, memo)
+  options.push(dontTakeCurrent)
+  if(prev < current){
+    const takeCurrent = 1 + maxIncreasingSubseq(numbers, i + 1, current, memo)
+    options.push(takeCurrent)
+  }
+  
+  memo[key] = Math.max(...options)
+  return memo[key]
+};
