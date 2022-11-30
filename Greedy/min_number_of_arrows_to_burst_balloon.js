f130f78bcd8279f62e4ea277883b60ac89dd96fa
HiThereImCam <ctanjoco@gmail.com>
2022-11-29 20:02:57 -0800

    find min arrows shots

--- Greedy/min_number_of_arrows_to_burst_balloon.js
+++ Greedy/min_number_of_arrows_to_burst_balloon.js
@@ -88,3 +88,77 @@ var findMinArrowShots = function(points) {
     
     return count
 };
+
+// 11/29/2022
+/*
+    a balloon with xStart and xEnd is BURST by an arrow shot at x if 
+        - xStart <= x <= xEnd
+    
+    no limit to number of arrows that can be shot
+        - travels infinitely
+        
+    return the min number of arrows that must be shot to burst all balloons
+
+         6
+         5
+         4             - - - -  -  - 
+         3 - - - - - -
+         2   - - - - - - -
+         1                   -  -  -  -  -  -  -
+         0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
+        
+        [[1,6], [2,8], [7,12], [10,16]] 
+    
+         6
+         5
+         4                   - - - - -  - - - - -
+         3             - - - -  -  -
+         2   - - - - - - -
+         1 - - - - - -                
+         0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 
+         
+         
+         
+         [[1,2], [3,4]] x = 2
+         
+         3            
+         2     - -
+         1 - -             
+         0 1 2 3 4
+         
+         start = [1,2] end = [3,4]
+         start[1] < end[0]
+            therefore you need two arrows
+            
+            
+        idea
+            - sort the array by the first element in the subarray in ascending order
+            - count = 1
+            - prev = points[0][1]
+            
+            iterate through the array
+                - is the currentEle[0] > prev
+                    - count += 1
+                    - prev = currentEle[1]
+                - else 
+                    - take min val of prev && currentEle[1]
+         
+         
+*/
+var findMinArrowShots = function(points) {
+    let sortedPoints = points.sort((a,b) => a[0] - b[0])
+     let count = 0
+     let prev = null
+     
+     for(let point of sortedPoints){
+         const [start, end] = point
+         if(prev === null || start > prev){
+             count += 1
+             prev = end
+         }else{
+             prev = Math.min(prev, end)
+         }
+     }
+     
+     return count
+ };
